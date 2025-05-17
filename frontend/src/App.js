0b65a0b2be589244f810fb3d7f5eed743e763797
SAGONGON <levinclimbing@gmail.com>
2025-05-17 05:09:25 +0300

    🚀 תיקון נתיב queue-scanner באמצעות React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import QueueScanner from './QueueScanner.js';
 import MainApp from './MainApp.js';
 
 const App = () => {
-  const location = useLocation();
-
-  if (location.pathname.startsWith('/queue-scanner/')) {
-    const stationId = location.pathname.split('/')[2];
-    return <QueueScanner stationId={stationId} />;
-  }
-
-  return <MainApp />;
+  return (
+    <Routes>
+      <Route path="/" element={<MainApp />} />
+      <Route path="/queue-scanner/:stationId" element={<QueueScanner />} />
+    </Routes>
+  );
 };
 
 export default App;
