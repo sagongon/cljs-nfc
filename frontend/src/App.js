6f4bc19864083a3fa9d3c93699d40615175f80ed
SAGONGON <levinclimbing@gmail.com>
2025-05-21 01:53:00 +0300

    📡 הוספת נתיב /live ל-App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainApp from './MainApp.js';
 import QueueScanner from './QueueScanner.js';
+import LiveBoard from './LiveBoard.js';
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<MainApp />} />
       <Route path="/queue-scanner/:stationId" element={<QueueScanner />} />
+      <Route path="/live" element={<LiveBoard />} />
     </Routes>
   </Router>
 );
